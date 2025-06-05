cb8f3dc812b73b67d5146c3f4534945de25a07c3
korra <korraemail@gmail.com>
2025-06-05 09:40:11 -0500

    Initial Commit

---
+++ navigtion/navigtionApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigtionApp.swift
+//  navigtion
+//
+//  Created by Scholar on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct navigtionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
